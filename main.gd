fc305eaa5c8c03bac5dd05c4758f965168caaa54
carlos padial <carlos@asturnazari.com>
2023-10-27 19:24:46 +0200

    first commit

---
+++ main.gd
@@ -0,0 +1,40 @@
+extends Node3D
+
+@onready var r_cont = $XROrigin3D/RightController
+@onready var l_cont = $XROrigin3D/LeftController
+@onready var fileDialogViewport = $FileSystemSelectDir/FileSystemWindow
+
+
+
+func _ready():
+	fileDialogViewport.connect_scene_signal("dir_selected", on_dir_selected)
+	
+	r_cont.connect("button_pressed", on_controller_button_pressed)
+	l_cont.connect("button_pressed", on_controller_button_pressed)
+
+func on_dir_selected(value):
+	print(value)
+	pass
+	
+func on_controller_button_pressed(event):
+#	print(event)
+	match event:
+		"ax_button":
+			print("---------------")
+			print(event)
+			var cancel_event = InputEventAction.new()
+			cancel_event.action = "ui_ok"
+			cancel_event.pressed = true
+			Input.parse_input_event(cancel_event)
+			pass
+		"by_button":
+			pass
+		"ax_touch":
+			pass
+		"by_touch":
+			pass
+		"trigger_click":
+			pass
+		"trigger_touch":
+			pass
+	
