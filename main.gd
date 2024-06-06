226e6825d0690d613d2105c558c7d6fed8dacdf9
carlos padial <carlos@asturnazari.com>
2024-06-06 10:10:41 +0200

    remove globals  singleton (not needed)

--- main.gd
+++ main.gd
@@ -4,46 +4,76 @@ extends Node3D
 @onready var l_cont = $XROrigin3D/LeftController
 @onready var fileDialogViewport = $FileSystemSelectDir/FileSystemWindow
 
+@onready var videoPlayer360 = $equi_2048/ViewportEquirectangular.get_scene_instance()
+@onready var videoPlayerFlat = $flat_screen/screen.get_scene_instance()
+
+var playlist := []
+var playListIndex := 0
+
 func _ready():
 	fileDialogViewport.connect_scene_signal("dir_selected", on_dir_selected)
 	
 	r_cont.connect("button_pressed", on_controller_button_pressed)
 	l_cont.connect("button_pressed", on_controller_button_pressed)
-	
-	Globals.connect("playlist_ready", on_playlist_ready)
+
+func dir_contents(path):
+	playlist = []
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				pass
+			else:
+				if file_name.ends_with(".ogv"):
+					createVideoStreamTheora(path + "/" + file_name)
+			file_name = dir.get_next()
+		on_playlist_ready()
+
+func createVideoStreamTheora(path):
+	var theoraResource = VideoStreamTheora.new()
+	theoraResource.set_file(ProjectSettings.globalize_path(path))
+	playlist.append(theoraResource)
+
+func nextIndex():
+	playListIndex += 1
+	if playListIndex > playlist.size() - 1:
+		playListIndex = 0
+	return playlist[playListIndex]
+
+func prevIndex():
+	playListIndex -= 1
+	if playListIndex < 0:
+		playListIndex = playlist.size() - 1
+	return playlist[playListIndex]
 
 func on_dir_selected(value):
-	print("holaaaaa", value)
-	Globals.dir_contents(value)
+	dir_contents(value)
 	await get_tree().create_timer(0.5).timeout
-	if Globals.playlist.size() > 0:
-		var videoPlayer360 = $equi_2048/ViewportEquirectangular.get_scene_instance()
-		var videoPlayerFlat = $flat_screen/screen.get_scene_instance()
-		videoPlayer360.loadStream(Globals.playlist[0])
-		videoPlayerFlat.loadStream(Globals.playlist[0])
+	if playlist.size() > 0:
+		videoPlayer360.loadStream(playlist[0])
+		videoPlayerFlat.loadStream(playlist[0])
 
 func on_playlist_ready():
-	print("playlist ready")
 	fileDialogViewport.scene_node.hide_file_dialog()
 	fileDialogViewport.scene_node.clear_file_list()
-	if Globals.playlist.size() > 0:
-		for file in Globals.playlist:
+	if playlist.size() > 0:
+		for file in playlist:
 			var array = file.get_file().rsplit("/")
 			var filename = array[array.size() - 1]
 			fileDialogViewport.scene_node.add_file_label(filename)
 
 func on_controller_button_pressed(event):
-	var videoPlayer360 = $equi_2048/ViewportEquirectangular.get_scene_instance()
-	var videoPlayerFlat = $flat_screen/screen.get_scene_instance()
 	match event:
 		"ax_button":
-			if Globals.playlist.size() > 0:
-				var stream = Globals.nextIndex()
+			if playlist.size() > 0:
+				var stream = nextIndex()
 				videoPlayer360.loadStream(stream)
 				videoPlayerFlat.loadStream(stream)
 		"by_button":
-			if Globals.playlist.size() > 0:
-				var stream = Globals.prevIndex()
+			if playlist.size() > 0:
+				var stream = prevIndex()
 				videoPlayer360.loadStream(stream)
 				videoPlayerFlat.loadStream(stream)
 		"ax_touch":
